e377c9248afd6259ef7336063f590f18b26e1d32
rowanmiller <mail@romiller.com>
2014-12-15 10:21:21 -0800

    Starting source and code snippets

---
+++ StartingSource/CycleSales/src/CycleSales.Tests/PriceServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using CycleSales.Models;
+using Microsoft.Data.Entity;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace CycleSales.Tests
+{
+    public class PriceServiceTests
+    {
+        [Fact]
+        public void Simple_price_increase()
+        {
+            /*
+            var options = new DbContextOptions<CycleSalesContext>()
+                .UseSqlServer(@"Server=.\SQLEXPRESS;Database=TestDatabase;Trusted_Connection=True;");
+
+            // Act
+            using (var db = new CycleSalesContext(options))
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                db.Bikes.Add(new Bike { Retail = 200 });
+                db.Bikes.Add(new Bike { Retail = 100 });
+                db.SaveChanges();
+            }
+
+            // Arrange
+            using (var db = new CycleSalesContext(options))
+            {
+                var svc = new PriceService(db);
+                svc.UpdatePrices(2);
+            }
+
+            // Assert
+            using (var db = new CycleSalesContext(options))
+            {
+                Assert.True(db.Bikes.Any(b => b.Retail == 400));
+                Assert.True(db.Bikes.Any(b => b.Retail == 200));
+            }
+            */
+        }
+    }
+}
---
+++ StartingSource/CycleSales/src/CycleSales/Models/CycleSalesContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using Microsoft.Data.Entity.Metadata;
+using System.Linq;
+
+namespace CycleSales.Models
+{
+    public class CycleSalesContext : DbContext
+    {
+        public DbSet<Bike> Bikes { get; set; }
+    }
+}
